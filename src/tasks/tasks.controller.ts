d30b8ef6a3bd858247b9805f7cb0ec34744d752f
Nithil N <nithiln96@gmail.com>
2020-05-02 19:53:28 +0530

    Creating a Post handler in the TasksController and consume the service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
@@ -10,5 +10,13 @@ export class TasksController {
   @Get()
   getAll(): Task[] {
     return this.tasksService.getAll();
+  }
+
+  @Post()
+  create(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.tasksService.create(title, description);
   }
 }
