fbaa6a75b2dd74828706675b2e031162a868b625
Nithil N <nithiln96@gmail.com>
2020-05-17 20:47:07 +0530

    Salt user password before saving into DB, installed bcrypt package

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,6 @@
-import { Repository, EntityRepository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import { Repository, EntityRepository } from 'typeorm';
 
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
@@ -8,9 +9,10 @@ import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
+    const salt = await bcrypt.genSalt();
 
     try {
-      await User.create({ username, password }).save();
+      await User.create({ username, salt, password: await this.hashPassword(password, salt) }).save();
     } catch (error) {
       const duplicateRecordCode = '23505';
       if (error.code === duplicateRecordCode) {
@@ -19,5 +21,9 @@ export class UserRepository extends Repository<User> {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
