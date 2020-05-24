58586220716a6bb018d8b55a1bf69c875c5f73be
Nithil N <nithiln96@gmail.com>
2020-05-24 18:10:49 +0530

    Setup relationship between task & user

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,5 +1,7 @@
 import * as bcrypt from 'bcrypt';
-import { BaseEntity, Entity, Column, PrimaryGeneratedColumn, Unique } from 'typeorm';
+import { BaseEntity, Entity, Column, PrimaryGeneratedColumn, Unique, OneToMany } from 'typeorm';
+
+import { Task } from '../tasks/task.entity';
 
 @Entity()
 @Unique(['username'])
@@ -15,6 +17,13 @@ export class User extends BaseEntity {
 
   @Column()
   salt: string;
+
+  @OneToMany(
+    type => Task,
+    task => task.user,
+    { eager: true },
+  )
+  tasks: Task[];
 
   async validatePassword(password: string): Promise<boolean> {
     const hash = await bcrypt.hash(password, this.salt);
--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,6 +1,7 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 
 import { TaskStatus } from './task-status.enum';
+import { User } from '../auth/user.entity';
 
 @Entity()
 export class Task extends BaseEntity {
@@ -15,4 +16,11 @@ export class Task extends BaseEntity {
 
   @Column()
   status: TaskStatus;
+
+  @ManyToOne(
+    type => User,
+    user => user.tasks,
+    { eager: false },
+  )
+  user: User;
 }
